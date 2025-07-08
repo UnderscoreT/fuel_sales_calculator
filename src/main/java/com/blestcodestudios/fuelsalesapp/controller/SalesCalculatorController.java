a975219aeba32cc2bdf18ff6dc7accf26409b0d6
Obey <obeyblessing@gmail.com>
2025-07-09 00:58:35 +0200

    download pdf

--- src/main/java/com/blestcodestudios/fuelsalesapp/controller/SalesCalculatorController.java
+++ src/main/java/com/blestcodestudios/fuelsalesapp/controller/SalesCalculatorController.java
@@ -1,13 +1,22 @@
 package com.blestcodestudios.fuelsalesapp.controller;
 
+import com.blestcodestudios.fuelsalesapp.service.PdfGenerator;
+import com.lowagie.text.DocumentException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class SalesCalculatorController {
 
-    double closingCash ;
+    double closingCash;
 
 //    @GetMapping("/")
 //    public String showCalculatorForm() {
@@ -51,9 +60,9 @@ public class SalesCalculatorController {
             @RequestParam(value = "couponLitresBlend", defaultValue = "0.0") double couponLitresBlend,
             @RequestParam(value = "couponLitresUnleaded", defaultValue = "0.0") double couponLitresUnleaded,
 
-            @RequestParam(value = "daCardLitresDiesel", defaultValue = "0.0")double daCardLitresDiesel,
-            @RequestParam(value = "daCardLitresBlend", defaultValue = "0.0")double daCardLitresBlend,
-            @RequestParam(value = "daCardLitresUnleaded", defaultValue = "0.0")double daCardLitresUnleaded,
+            @RequestParam(value = "daCardLitresDiesel", defaultValue = "0.0") double daCardLitresDiesel,
+            @RequestParam(value = "daCardLitresBlend", defaultValue = "0.0") double daCardLitresBlend,
+            @RequestParam(value = "daCardLitresUnleaded", defaultValue = "0.0") double daCardLitresUnleaded,
 
             @RequestParam(value = "cashDropped", defaultValue = "0.0") double cashDropped,
 
@@ -77,13 +86,13 @@ public class SalesCalculatorController {
 
         double blendNetLitres = litresBlend - couponLitresBlend - daCardLitresBlend;
         double dieselNetLitres = litresDiesel - couponLitresDiesel - daCardLitresDiesel;
-        double unleadedNetLitres = litresUnleaded - couponLitresUnleaded -daCardLitresUnleaded;
+        double unleadedNetLitres = litresUnleaded - couponLitresUnleaded - daCardLitresUnleaded;
 
-        double totalRevenue = litresBlend*pricePerLitreBlend+ litresUnleaded*pricePerLitreUnleaded+litresDiesel*pricePerLitreDiesel;
-        double totalRevenueInCash = litresDiesel*pricePerLitreDiesel + litresUnleaded*pricePerLitreUnleaded+
-                litresBlend*pricePerLitreBlend - couponLitresBlend*pricePerLitreBlend - couponLitresDiesel*pricePerLitreDiesel
-                -couponLitresUnleaded*pricePerLitreUnleaded -daCardLitresBlend*pricePerLitreBlend
-                -daCardLitresDiesel*pricePerLitreDiesel -daCardLitresUnleaded*pricePerLitreUnleaded;
+        double totalRevenue = litresBlend * pricePerLitreBlend + litresUnleaded * pricePerLitreUnleaded + litresDiesel * pricePerLitreDiesel;
+        double totalRevenueInCash = litresDiesel * pricePerLitreDiesel + litresUnleaded * pricePerLitreUnleaded +
+                litresBlend * pricePerLitreBlend - couponLitresBlend * pricePerLitreBlend - couponLitresDiesel * pricePerLitreDiesel
+                - couponLitresUnleaded * pricePerLitreUnleaded - daCardLitresBlend * pricePerLitreBlend
+                - daCardLitresDiesel * pricePerLitreDiesel - daCardLitresUnleaded * pricePerLitreUnleaded;
         double cashInHand = totalRevenueInCash - cashDropped;
         closingCash = cashInHand;
 
@@ -117,4 +126,90 @@ public class SalesCalculatorController {
 //        return "result";
         return "sales-summary";
     }
+
+    @Autowired
+    private PdfGenerator pdfGenerator;
+
+    @PostMapping("/summary/pdf")
+    public ResponseEntity<byte[]> downloadSalesSummaryPdf(
+                                                          @RequestParam("startReadingBlendA") double startReadingBlendA,
+                                                          @RequestParam("endReadingBlendA") double endReadingBlendA,
+                                                          @RequestParam("startReadingBlendB") double startReadingBlendB,
+                                                          @RequestParam("endReadingBlendB") double endReadingBlendB,
+                                                          @RequestParam("startReadingDieselA") double startReadingDieselA,
+                                                          @RequestParam("endReadingDieselA") double endReadingDieselA,
+                                                          @RequestParam("startReadingDieselB") double startReadingDieselB,
+                                                          @RequestParam("endReadingDieselB") double endReadingDieselB,
+                                                          @RequestParam("startReadingUnleadedA") double startReadingUnleadedA,
+                                                          @RequestParam("endReadingUnleadedA") double endReadingUnleadedA,
+                                                          @RequestParam("startReadingUnleadedA") double startReadingUnleadedB,
+                                                          @RequestParam("endReadingUnleadedB") double endReadingUnleadedB,
+                                                          @RequestParam("pricePerLitreDiesel") double pricePerLitreDiesel,
+                                                          @RequestParam("pricePerLitreBlend") double pricePerLitreBlend,
+                                                          @RequestParam("pricePerLitreUnleaded") double pricePerLitreUnleaded,
+
+                                                          @RequestParam(value = "couponLitresDiesel", defaultValue = "0.0") double couponLitresDiesel,
+                                                          @RequestParam(value = "couponLitresBlend", defaultValue = "0.0") double couponLitresBlend,
+                                                          @RequestParam(value = "couponLitresUnleaded", defaultValue = "0.0") double couponLitresUnleaded,
+
+                                                          @RequestParam(value = "daCardLitresDiesel", defaultValue = "0.0") double daCardLitresDiesel,
+                                                          @RequestParam(value = "daCardLitresBlend", defaultValue = "0.0") double daCardLitresBlend,
+                                                          @RequestParam(value = "daCardLitresUnleaded", defaultValue = "0.0") double daCardLitresUnleaded,
+
+                                                          @RequestParam(value = "cashDropped", defaultValue = "0.0") double cashDropped) throws DocumentException
+
+
+        {
+            double litresBlendA = endReadingBlendA - startReadingBlendA;
+            double litresBlendB = endReadingBlendB - startReadingBlendB;
+            double litresDieselA = endReadingDieselA - startReadingDieselA;
+            double litresDieselB = endReadingDieselB - startReadingDieselB;
+            double litresUnleadedA = endReadingUnleadedA - startReadingUnleadedA;
+            double litresUnleadedB = endReadingUnleadedB - startReadingUnleadedB;
+
+            double totalLitres = litresBlendA + litresBlendB + litresDieselA + litresDieselB + litresUnleadedA + litresUnleadedB;
+            double couponLitres = couponLitresBlend + couponLitresUnleaded + couponLitresDiesel;
+            double daCardLitres = daCardLitresBlend + daCardLitresUnleaded + daCardLitresDiesel;
+
+            double netLitres = totalLitres - couponLitres - daCardLitres;
+            double litresBlend = litresBlendA + litresBlendB;
+            double litresDiesel = litresDieselA + litresDieselB;
+            double litresUnleaded = litresUnleadedA + litresUnleadedB;
+
+            double totalRevenue = litresBlend * pricePerLitreBlend + litresUnleaded * pricePerLitreUnleaded + litresDiesel * pricePerLitreDiesel;
+            double totalRevenueInCash = litresDiesel * pricePerLitreDiesel + litresUnleaded * pricePerLitreUnleaded +
+                    litresBlend * pricePerLitreBlend - couponLitresBlend * pricePerLitreBlend - couponLitresDiesel * pricePerLitreDiesel
+                    - couponLitresUnleaded * pricePerLitreUnleaded - daCardLitresBlend * pricePerLitreBlend
+                    - daCardLitresDiesel * pricePerLitreDiesel - daCardLitresUnleaded * pricePerLitreUnleaded;
+            double cashInHand = totalRevenueInCash - cashDropped;
+
+            Map<String, Object> data = new HashMap<>();
+            data.put("litresBlend", litresBlend);
+            data.put("litresDiesel", litresDiesel);
+            data.put("litresUnleaded", litresUnleaded);
+            data.put("netLitres", netLitres);
+            data.put("totalLitres", totalLitres);
+            data.put("couponLitres", couponLitres);
+            data.put("daCardLitres", daCardLitres);
+            data.put("daCardLitresBlend", daCardLitresBlend);
+            data.put("daCardLitresUnleaded", daCardLitresUnleaded);
+            data.put("daCardLitresDiesel", daCardLitresDiesel);
+            data.put("couponLitresDiesel", couponLitresDiesel);
+            data.put("couponLitresUnleaded", couponLitresUnleaded);
+            data.put("couponLitresBlend", couponLitresBlend);
+            data.put("cashDropped", cashDropped);
+            data.put("totalRevenue", totalRevenue);
+            data.put("totalRevenueInCash", totalRevenueInCash);
+            data.put("cashInHand", cashInHand);
+
+            byte[] pdfBytes = pdfGenerator.generatePdf("sales-summary", data);
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"fuel-sales-summary.pdf\"")
+                    .body(pdfBytes);
+        }
+
+
+
 }
---
+++ src/main/java/com/blestcodestudios/fuelsalesapp/service/PdfGenerator.java
@@ -0,0 +1,40 @@
+package com.blestcodestudios.fuelsalesapp.service;
+
+import com.blestcodestudios.fuelsalesapp.util.Logger;
+import com.lowagie.text.DocumentException;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Map;
+
+@Service
+public class PdfGenerator {
+
+    private final TemplateEngine templateEngine;
+
+    public PdfGenerator(TemplateEngine templateEngine) {
+        this.templateEngine = templateEngine;
+    }
+
+    public byte[] generatePdf(String templateName, Map<String, Object> data) throws DocumentException {
+        Context context = new Context();
+        context.setVariables(data);
+
+        String htmlContent = templateEngine.process(templateName, context);
+
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            ITextRenderer renderer = new ITextRenderer();
+            renderer.setDocumentFromString(htmlContent);
+            renderer.layout();
+            renderer.createPDF(outputStream);
+            return outputStream.toByteArray();
+        } catch (Exception e) {
+            Logger.getLogger().printMessage(e.getMessage());
+            throw new DocumentException(e);
+
+        }
+    }
+}
